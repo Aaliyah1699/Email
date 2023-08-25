3f4257438f8da484c0ad6ff816a909fc4eebe995
Aaliyah <93296412+Aaliyah1699@users.noreply.github.com>
2023-08-25 16:13:29 -0500

    Send fake email using Nodemailer & ethereal email

--- controllers/sendEmail.js
+++ controllers/sendEmail.js
@@ -1,5 +1,24 @@
+const nodemailer = require("nodemailer");
+
 const sendEmail = async (req, res) => {
-    res.send("send email");
+    const testAccount = await nodemailer.createTestAccount();
+
+    const transporter = nodemailer.createTransport({
+        host: "smtp.ethereal.email",
+        port: 587,
+        auth: {
+            user: "magnolia.kozey37@ethereal.email",
+            pass: "651ak7btk5sFjX566P",
+        },
+    });
+
+    const info = await transporter.sendMail({
+        from: '"Eleazar Goyette"<eleazar62@ethereal.email>',
+        to: "baz@example.com",
+        subject: "Send fake email",
+        html: "<h2>Sending emails with Node.JS</h2>",
+    });
+    res.json(info);
 };
 
 module.exports = sendEmail;
